72c863833735c74025d153bd465861aa0a6e4441
teihong93 <teihong93@gmail.com>
2022-02-02 05:21:17 +0900

    feat: repl 에서 파싱할 수 있도록 수정

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -1,14 +1,64 @@
 import {createInterface} from 'readline';
 import {Lexer} from '../lexer/lexter';
 import tokenPool from '../token/tokenPool';
+import {Parser} from '../parser/parser';
+import {assert} from 'chai';
 
 let rl = createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
+const deLexRepl = () => {
+    const query = () => {
+        rl.question('lexing start... please enter code. [q] to quit\n', (answer) => {
+            switch (answer.toLowerCase()) {
+                case 'q':
+                    console.log('Bye!');
+                    rl.close();
+                    break;
+                default: {
+                    let lexer = Lexer({input: answer});
+                    for (let t = lexer.nextToken(); t.type != tokenPool.EOF; t = lexer.nextToken()) {
+                        console.log(t);
+                    }
+                    query();
+                }
+            }
+        });
+    };
+    query();
+};
+
+const doParseRepl = () => {
+    const query = () => {
+        rl.question('parsing start... please enter code. [q] to quit\n', (answer) => {
+            switch (answer.toLowerCase()) {
+                case 'q':
+                    console.log('Bye!');
+                    rl.close();
+                    break;
+                default: {
+                    let lexer = Lexer({input: answer});
+                    let parser = Parser({lexer})
+                    const program = parser.parseProgram()
+                    const errors = parser.errors();
+                    if (errors.length !== 0) {
+                        const errMsg = errors.reduce((acc, e) => (acc + `\nerr: ${e}\n`), '');
+                        console.log(`\n파서에서 ${errors.length} 개의 에러 발견 ${errMsg}`);
+                    } else {
+                        console.log(program.string())
+                    }
+                    query();
+                }
+            }
+        });
+    };
+    query();
+};
+
 export function runRepl() {
-    console.log(' TeiScript REPL , press q to quit \n');
+    console.log(' TeiScript REPL , \npress [q] to quit \n[lex] to start lexing \n[parse] to start parse\n');
     const query = () => {
         rl.question('>>>', (answer) => {
             switch (answer.toLowerCase()) {
@@ -16,14 +66,18 @@ export function runRepl() {
                     console.log('Bye!');
                     rl.close();
                     break;
+                case 'lex':
+                    deLexRepl();
+                    break;
+                case 'parse':
+                    doParseRepl();
+                    break;
                 default:
-                    const lexer = Lexer({input: answer});
-                    for (let t = lexer.nextToken(); t.type != tokenPool.EOF; t = lexer.nextToken()) {
-                        console.log(t);
-                    }
-                    query();
+                    break;
             }
         });
     };
     query();
 }
+
+
